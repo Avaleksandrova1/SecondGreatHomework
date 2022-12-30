b511243ee58e0393b40599274aeea3f5d09e955a
Avaleksandrova1 <avaleksandrova_6@edu.hse.ru>
2022-12-30 04:36:44 +0300

    Модификация1.Вложенность.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import RootFolderHandler.FilesConnector;
 
 public class Main {
     public static void main(String[] args) {
-        FilesConnector taskDirectoryFilesConnector = new FilesConnector("/Users/Nastya/kpoGreathomeworks/second/folder_for_task");
+        FilesConnector  taskDirectoryFilesConnector= new FilesConnector("/Users/Nastya/kpoGreathomeworks/second/folder_for_task");
 
     }
 
--- src/RootFolderHandler/FilesConnector.java
+++ src/RootFolderHandler/FilesConnector.java
@@ -1,15 +1,36 @@
 package RootFolderHandler;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class FilesConnector {
+    private final Path pathDirectory;
+
     public FilesConnector(String rootFolderPath) {
-        Path pathDirectory = Paths.get(rootFolderPath);
+        pathDirectory = Paths.get(rootFolderPath);
         if (Files.notExists(pathDirectory)) {
-            System.err.println("Возникли проблемы с путем коревой папки.");
+            System.err.println("Возникли проблемы с путем корневой папки.");
+        }
+        try {
+            List<Path> directoryFiles = getFiles();
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
         }
 
-
     }
+    private List<Path> getFiles() throws IOException {
+        List<Path> listFilePaths;
+        try (Stream<Path> walk = Files.walk(pathDirectory)) {
+            listFilePaths = walk.filter(Files::isRegularFile).collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new IOException("Произошла ошибка при попытке поиска файлов в корневой папке.");
+        }
+        return listFilePaths;
+    }
+
 }
